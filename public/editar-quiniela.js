9bfa079c301ceab36c75d61fff22c7315b7212dd
Armenta <macbookpro@MacBook-Pro-de-Armenta.local>
2026-07-14 00:34:35 -0600

    colores botones quiniela

--- public/editar-quiniela.js
+++ public/editar-quiniela.js
@@ -3,3 +3,36 @@ if(sessionStorage.getItem("admin") !== "true"){
     location.href="login.html";
 
 }
+
+async function cargarJornadas(){
+
+    const select = document.getElementById("jornadaEditar");
+
+    select.innerHTML = "";
+
+    for(let i = 1; i <= 60; i++){
+
+        select.innerHTML += `
+            <option value="${i}">
+                Semana ${i}
+            </option>
+        `;
+
+    }
+
+    try{
+
+        const r = await fetch("/jornada-actual");
+        const data = await r.json();
+
+        select.value = data.jornada;
+
+    }catch(err){
+
+        select.value = 1;
+
+    }
+
+}
+
+cargarJornadas();
